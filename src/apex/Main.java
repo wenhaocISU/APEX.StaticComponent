da1ca99f806df070a455e6ccb213e9903e40f8bd
xjtujack <xjtujack@gmail.com>
2015-09-21 23:54:01 -0500

    first commit

--- src/apex/Main.java
+++ src/apex/Main.java
@@ -1,18 +1,7 @@
 package apex;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-
-import apex.parser.SmaliParser;
 import apex.staticFamily.StaticApp;
 import apex.staticFamily.StaticAppBuilder;
-import apex.staticFamily.StaticClass;
-import apex.staticFamily.StaticMethod;
-import apex.staticFamily.StaticStmt;
 
 public class Main {
 
@@ -52,69 +41,5 @@ public class Main {
 
 	}
 	
-	private static void debugging(StaticApp staticApp)
-	{
-		for (StaticClass c : staticApp.getClasses())
-		{
-			ArrayList<String> originalSmali = new ArrayList<String>();
-			try
-			{
-				BufferedReader in = new BufferedReader(new FileReader(c.getSmaliFilePath()));
-				String line;
-				while ((line = in.readLine())!= null)
-				{
-					originalSmali.add(line);
-				}
-				in.close();
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-			ArrayList<String> oldSmali = c.getBody();
-			ArrayList<String> newSmali = c.getInstrumentedBody();
-			
-			int original = 0, old = 0, New = 0;
-			for (String line : originalSmali)
-			{
-				if (line.startsWith("    :array_") || line.startsWith("    :pswitch_data_") || line.startsWith("    :sswitch_data_"))
-					original++;
-			}
-			for (String line : oldSmali)
-			{
-				if (line.startsWith("    :array_") || line.startsWith("    :pswitch_data_") || line.startsWith("    :sswitch_data_"))
-					old++;
-			}
-			for (String line : newSmali)
-			{
-				if (line.startsWith("    :array_") || line.startsWith("    :pswitch_data_") || line.startsWith("    :sswitch_data_"))
-					New++;
-			}
-			if (original != old || original != New || old != New)
-				System.out.println("======== " + c.getDexName());
-		}
-	}
-	
-	private static void testClass()
-	{
-		String path = "/home/wenhaoc/AppData/" +
-		"AndBible.apk/apktool/oldSmali/org/apache/lucene/index/DirectoryReader.smali";
-		File f = new File(path);
-		SmaliParser sp = new SmaliParser(path);
-		try
-		{
-			StaticClass c = sp.Parse();
-			PrintWriter out = new PrintWriter("/home/wenhaoc/test.smali");
-			for (String s : c.getInstrumentedBody())
-			{
-				out.write(s + "\n");
-			}
-			out.flush();
-			out.close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
+
 }
